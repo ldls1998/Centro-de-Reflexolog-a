a7cf2f9fbd00348a7a6a436b688547dca2b62e4b
Leonardo Loaiza <leonardodario.loaiza@unmsm.edu.pe>
2022-12-16 17:30:59 -0500

    g110 interfaz básica y creación de g120

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.reflexologia {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.reflexologia to javafx.fxml;
     exports com.mycompany.reflexologia;
